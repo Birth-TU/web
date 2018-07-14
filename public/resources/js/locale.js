713a874cb79a132626c4f0dc09f241b0c0a90ac5
Wiput Pootong <wiput.pootong@gmail.com>
2018-07-14 17:49:50 +0700

    :bug: fix rayriffy!!!!!@$@#%@!%@#$^$#*#$^@#$^@#^$

--- public/resources/js/locale.js
+++ public/resources/js/locale.js
@@ -12,7 +12,7 @@ const loadJSON = (lang) => new Promise((resolve, reject) => {
 
   var xobj = new XMLHttpRequest();
   xobj.overrideMimeType("application/json");
-  xobj.open('GET', `../locale/${lang}.json`, true); // Replace 'my_data' with the path to your file
+  xobj.open('GET', `/resources/locale/${lang}.json`, true); // Replace 'my_data' with the path to your file
   xobj.onreadystatechange = () => {
     if (xobj.readyState == 4 && xobj.status == "200") {
       // Required use of an anonymous callback as .open will NOT return a value but simply returns undefined in asynchronous mode
