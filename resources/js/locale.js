8f2e4d1677409f4abd72f1a4547172beff4c7984
Tanyawat Vittayapalotai <hamzaabamboo@gmail.com>
2018-10-01 13:20:00 +0700

    :bug: make path relative

--- resources/js/locale.js
+++ resources/js/locale.js
@@ -1,56 +1,53 @@
 const getParameterByName = (name, url) => {
   if (!url) url = window.location.href;
-  name = name.replace(/[\[\]]/g, '\\$&');
-  var regex = new RegExp('[?&]' + name + '(=([^&#]*)|&|#|$)'),
+  name = name.replace(/[\[\]]/g, "\\$&");
+  var regex = new RegExp("[?&]" + name + "(=([^&#]*)|&|#|$)"),
     results = regex.exec(url);
   if (!results) return null;
-  if (!results[2]) return '';
-  return decodeURIComponent(results[2].replace(/\+/g, ' '));
-}
+  if (!results[2]) return "";
+  return decodeURIComponent(results[2].replace(/\+/g, " "));
+};
 
-const loadJSON = (lang) => new Promise((resolve, reject) => {
-
-  var xobj = new XMLHttpRequest();
-  xobj.overrideMimeType("application/json");
-  xobj.open('GET', `/resources/locale/${lang}.json`, true); // Replace 'my_data' with the path to your file
-  xobj.onreadystatechange = () => {
-    if (xobj.readyState == 4 && xobj.status == "200") {
-      // Required use of an anonymous callback as .open will NOT return a value but simply returns undefined in asynchronous mode
-      resolve(xobj.responseText);
-    }
-  };
-  xobj.send(null);
-})
-
+const loadJSON = lang =>
+  new Promise((resolve, reject) => {
+    var xobj = new XMLHttpRequest();
+    xobj.overrideMimeType("application/json");
+    xobj.open("GET", `resources/locale/${lang}.json`, true); // Replace 'my_data' with the path to your file
+    xobj.onreadystatechange = () => {
+      if (xobj.readyState == 4 && xobj.status == "200") {
+        // Required use of an anonymous callback as .open will NOT return a value but simply returns undefined in asynchronous mode
+        resolve(xobj.responseText);
+      }
+    };
+    xobj.send(null);
+  });
 
 async function main() {
-
-  let locale = '';
-  if (getParameterByName('lang') !== null) {
-    locale = getParameterByName('lang');
+  let locale = "";
+  if (getParameterByName("lang") !== null) {
+    locale = getParameterByName("lang");
   } else locale = navigator.language.substring(0, 2) || "en";
 
   const result = JSON.parse(await loadJSON(locale));
 
-  let current_year = (new Date()).getFullYear();
+  let current_year = new Date().getFullYear();
 
   let elements = {
-    'subtitle': `${result.BODY1}`,
-    'lltk': `${result.LLTK}`,
-    'current_year': `${result.BODY2}${current_year + 543} ${result.BODY3}`,
-    'title_n': `${result.MONTH}`,
-    'title_d': `${result.DAY}`,
-    'title_h': `${result.HOUR}`,
-    'title_m': `${result.MINUTE}`,
-    'title_s': `${result.SECOND}`,
-    'head': `${result.TITLE}`,
-    'title': `${result.TITLE}`,
-    'source': `${result.SOURCE}`,
-    'darkmode': `${result.DARKMODE}`
-  }
+    subtitle: `${result.BODY1}`,
+    lltk: `${result.LLTK}`,
+    current_year: `${result.BODY2}${current_year + 543} ${result.BODY3}`,
+    title_n: `${result.MONTH}`,
+    title_d: `${result.DAY}`,
+    title_h: `${result.HOUR}`,
+    title_m: `${result.MINUTE}`,
+    title_s: `${result.SECOND}`,
+    head: `${result.TITLE}`,
+    title: `${result.TITLE}`,
+    source: `${result.SOURCE}`,
+    darkmode: `${result.DARKMODE}`
+  };
 
-
-  Object.keys(elements).forEach((element) => {
+  Object.keys(elements).forEach(element => {
     document.getElementById(element).innerHTML = elements[element];
   });
 
@@ -66,31 +63,29 @@ async function main() {
   // document.getElementById('title').appendChild(document.createTextNode(`${result.TITLE}`));
   // document.getElementById('source').appendChild(document.createTextNode(`${result.SOURCE}`));
   // document.getElementById('darkmode').appendChild(document.createTextNode(`${result.DARKMODE}`));
-
 }
 
 async function update(lang) {
   const result = JSON.parse(await loadJSON(lang));
 
-  let current_year = (new Date()).getFullYear();
+  let current_year = new Date().getFullYear();
 
   let elements = {
-    'subtitle': `${result.BODY1}`,
-    'lltk': `${result.LLTK}`,
-    'current_year': `${result.BODY2}${current_year + 543} ${result.BODY3}`,
-    'title_n': `${result.MONTH}`,
-    'title_d': `${result.DAY}`,
-    'title_h': `${result.HOUR}`,
-    'title_m': `${result.MINUTE}`,
-    'title_s': `${result.SECOND}`,
-    'head': `${result.TITLE}`,
-    'title': `${result.TITLE}`,
-    'source': `${result.SOURCE}`,
-    'darkmode': `${result.DARKMODE}`
-  }
+    subtitle: `${result.BODY1}`,
+    lltk: `${result.LLTK}`,
+    current_year: `${result.BODY2}${current_year + 543} ${result.BODY3}`,
+    title_n: `${result.MONTH}`,
+    title_d: `${result.DAY}`,
+    title_h: `${result.HOUR}`,
+    title_m: `${result.MINUTE}`,
+    title_s: `${result.SECOND}`,
+    head: `${result.TITLE}`,
+    title: `${result.TITLE}`,
+    source: `${result.SOURCE}`,
+    darkmode: `${result.DARKMODE}`
+  };
 
-
-  Object.keys(elements).forEach((element) => {
+  Object.keys(elements).forEach(element => {
     document.getElementById(element).innerHTML = elements[element];
   });
 }
