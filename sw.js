6e55c6b5f1061bb1be797632c982f88cb0ecae92
Phumrapee Limpianchop <git@rayriffy.com>
2018-10-01 21:30:39 +0700

    :sparkles: Workbox JS

--- sw.js
+++ sw.js
@@ -1,77 +1,38 @@
-var EXTRA_FILES = [];
+importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.0.0/workbox-sw.js")
 
-var CHECKSUM = "v1";
+workbox.setConfig({
+  debug: false
+})
 
-var FILES = [
-  '/',
-  '/?lang=th',
-  '/?lang=en',
-  '/?lang=de',
-  'https://fontuni.com/boon/css/boon-all.css',
-  '/index.html',
-  '/index.html?lang=th',
-  '/index.html?lang=en',
-  '/index.html?lang=de',
-  '/resources/icon/apple-touch-icon-57x57.png',
-  '/resources/icon/apple-touch-icon-114x114.png',
-  '/resources/icon/apple-touch-icon-72x72.png',
-  '/resources/icon/apple-touch-icon-144x144.png',
-  '/resources/icon/apple-touch-icon-60x60.png',
-  '/resources/icon/apple-touch-icon-120x120.png',
-  '/resources/icon/apple-touch-icon-76x76.png',
-  '/resources/icon/apple-touch-icon-152x152.png',
-  '/resources/icon/favicon-196x196.png',
-  '/resources/icon/favicon-96x96.png',
-  '/resources/icon/favicon-32x32.png',
-  '/resources/icon/favicon-16x16.png',
-  '/resources/icon/favicon-128.png',
-  '/resources/icon/mstile-144x144.png',
-  '/resources/icon/mstile-70x70.png',
-  '/resources/icon/mstile-150x150.png',
-  '/resources/icon/mstile-310x150.png',
-  '/resources/icon/mstile-310x310.png',
-  '/resources/css/firework.css',
-  '/resources/css/main.css',
-  '/resources/misc/song.mp3',
-  '/resources/icon/th-TH.png',
-  '/resources/icon/en-US.png',
-  '/resources/icon/de-DE.png',
-  '/resources/js/locale.js',
-  '/resources/js/darkmode.js',
-  '/resources/js/main.js',
-  '/resources/locale/th.json',
-  '/resources/locale/en.json',
-  '/resources/locale/de.json'
-].concat(EXTRA_FILES || []);
+workbox.routing.registerRoute(
+  new RegExp('.*(?:googleapis|gstatic)\.com.*$'),
+  workbox.strategies.staleWhileRevalidate(),
+)
 
-var CACHENAME = 'birth-tu-' + CHECKSUM;
+workbox.routing.registerRoute(
+  new RegExp('.*\.js'),
+  workbox.strategies.staleWhileRevalidate({
+    cacheName: 'js-cache',
+  })
+)
 
-self.addEventListener('install', function (event) {
-  event.waitUntil(caches.open(CACHENAME).then(function (cache) {
-    return cache.addAll(FILES);
-  }));
-});
+workbox.routing.registerRoute(
+  new RegExp('.*\.css'),
+  workbox.strategies.staleWhileRevalidate({
+    cacheName: 'css-cache',
+  })
+)
 
-self.addEventListener('activate', function (event) {
-  return event.waitUntil(caches.keys().then(function (keys) {
-    return Promise.all(keys.map(function (k) {
-      if (k != CACHENAME && k.indexOf('birth-tu-') == 0) {
-        return caches.delete(k);
-      } else {
-        return Promise.resolve();
-      }
-    }));
-  }));
-});
+workbox.routing.registerRoute(
+  new RegExp('.*\.woff2'),
+  workbox.strategies.staleWhileRevalidate({
+    cacheName: 'font-cache',
+  })
+)
 
-self.addEventListener('fetch', event => {
-  event.respondWith(
-    caches.match(event.request)
-    .then(response => response || fetch(event.request))
-    .catch(() => {
-      if (event.request.mode == 'navigate') {
-        return caches.match('/offline.html');
-      }
-    })
-  );
-});
+workbox.routing.registerRoute(
+  /\.(?:png|gif|jpg|jpeg|svg|ico)$/,
+  workbox.strategies.staleWhileRevalidate({
+    cacheName: 'image-cache',
+  })
+)
