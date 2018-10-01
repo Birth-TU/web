fc41d66214146a42fe077775af34d94550b4ed9e
Phumrapee Limpianchop <git@rayriffy.com>
2018-10-01 21:35:56 +0700

    :package: Minify files

--- resources/js/darkmode.js
+++ resources/js/darkmode.js
@@ -1,9 +1,1 @@
-function darkmode() {
-  if (!document.getElementById('container').style.filter) {
-    document.getElementById('container').style.filter = 'grayscale(1) invert(1)';
-    document.getElementById('boon-300').style.backgroundColor = '#000';
-  } else {
-    document.getElementById('container').style.filter = '';
-    document.getElementById('boon-300').style.backgroundColor = '';
-  }
-}
+function darkmode(){document.getElementById("container").style.filter?(document.getElementById("container").style.filter="",document.getElementById("boon-300").style.backgroundColor=""):(document.getElementById("container").style.filter="grayscale(1) invert(1)",document.getElementById("boon-300").style.backgroundColor="#000")}
--- sw.js
+++ sw.js
@@ -1,38 +1,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.0.0/workbox-sw.js")
-
-workbox.setConfig({
-  debug: false
-})
-
-workbox.routing.registerRoute(
-  new RegExp('.*(?:googleapis|gstatic)\.com.*$'),
-  workbox.strategies.staleWhileRevalidate(),
-)
-
-workbox.routing.registerRoute(
-  new RegExp('.*\.js'),
-  workbox.strategies.staleWhileRevalidate({
-    cacheName: 'js-cache',
-  })
-)
-
-workbox.routing.registerRoute(
-  new RegExp('.*\.css'),
-  workbox.strategies.staleWhileRevalidate({
-    cacheName: 'css-cache',
-  })
-)
-
-workbox.routing.registerRoute(
-  new RegExp('.*\.woff2'),
-  workbox.strategies.staleWhileRevalidate({
-    cacheName: 'font-cache',
-  })
-)
-
-workbox.routing.registerRoute(
-  /\.(?:png|gif|jpg|jpeg|svg|ico)$/,
-  workbox.strategies.staleWhileRevalidate({
-    cacheName: 'image-cache',
-  })
-)
+workbox.setConfig({debug:!1})
+workbox.routing.registerRoute(new RegExp('.*(?:googleapis|gstatic)\.com.*$'),workbox.strategies.staleWhileRevalidate(),)
+workbox.routing.registerRoute(new RegExp('.*\.js'),workbox.strategies.staleWhileRevalidate({cacheName:'js-cache',}))
+workbox.routing.registerRoute(new RegExp('.*\.css'),workbox.strategies.staleWhileRevalidate({cacheName:'css-cache',}))
+workbox.routing.registerRoute(new RegExp('.*\.woff2'),workbox.strategies.staleWhileRevalidate({cacheName:'font-cache',}))
+workbox.routing.registerRoute(/\.(?:png|gif|jpg|jpeg|svg|ico)$/,workbox.strategies.staleWhileRevalidate({cacheName:'image-cache',}))
